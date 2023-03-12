b43d144d92227d538c336e0b80c55945337d3ba6
ooNonG <lze0603@gmail.com>
2023-03-12 14:06:31 +0100

    2023/03/12 Assignmt1

--- 111000272_農政宇_hw1.py
+++ 111000272_農政宇_hw1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""report balance after input initial amount of money and expenses record"""
 
 balance = int(input("How much money do you have? "))
 expensesRec = input("Add an expense or income record with description and amount:\n")
